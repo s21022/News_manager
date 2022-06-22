3978854c1189d46176333271ae22940fbb45b195
s21022 <s21022@std.it-college.ac.jp>
2022-06-22 10:33:05 +0900

    Initial commit

---
+++ src/main/kotlin/jp/ac/it_college/std/s21022/news_manager/NewsManagerApplication.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.std.s21022.news_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NewsManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<NewsManagerApplication>(*args)
+}
---
+++ src/test/kotlin/jp/ac/it_college/std/s21022/news_manager/NewsManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.ac.it_college.std.s21022.news_manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewsManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
